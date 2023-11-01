1d9b83b5623d55ef8fd288b4170914c9ee5e0550
Jonathon Reese Perry <jreesep@gmail.com>
2023-11-01 12:41:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@ sudo apt update
 sudo apt install fish
 
 # install ll on steroids
-sudo apt install exa
+apt-get install exa
 
 # install stow dotfiles manager
-sudo apt-get install -y stow
+apt-get install -y stow
 
 # stow files in .dotfiles
 stow fish
-stow vim
+# stow vim
 
 # install vim
-sudo apt install vim
+# apt-get install vim
 
 # install nodejs
 # curl -0- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
@@ -28,15 +28,15 @@ sudo apt install vim
 # install npm dev tools
 # sudo npm install -g typescript sass gulp
 
-command -v fish | sudo tree -a /ect/shells
+# command -v fish | sudo tree -a /ect/shells
 
 # set default shell to fish
 sudo chsh -s $(which fish) $USER
 
 # install fisher fish minimal package manager
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+# curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
 # install plugins
 sudo fisher update
 
-vim -c ":PlugInstall"
+# vim -c ":PlugInstall"
