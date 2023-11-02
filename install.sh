3514564b675be38cdbdf541e4608428e2d74cc8a
Jonathon Reese Perry <jreesep@gmail.com>
2023-11-01 20:13:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,30 +6,30 @@ sudo apt-get update -q --no-allow-insecure-repositories && sudo apt-get install 
 
 # font install
 # nerdfont fonts for fish shell theme
-sudo apt-get update -q --no-allow-insecure-repositories && sudo apt-get install fontconfig -y --no-install-recommends
+# sudo apt-get update -q --no-allow-insecure-repositories && sudo apt-get install fontconfig -y --no-install-recommends
 
-font_list=("DaddyTimeMono")
-for font_name in $font_list
-do
-  echo "Starting download ${font_name} nerd font"
-  if [ -n "$font_name" ]; then
-      echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${font_name}.zip"
-      curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${font_name}.zip"
-      echo "creating fonts folder: ~/.local/share/fonts"
-      mkdir ~/.local/share/fonts
-      echo "unzip the ${font_name}.zip"
-      unzip "${font_name}.zip" -d ~/.local/share/fonts/
-      fc-cache ~/.local/share/fonts
-      rm -rf "${font_name}.zip"
-      echo "done!"
+# font_list=("DaddyTimeMono")
+# for font_name in $font_list
+# do
+#   echo "Starting download ${font_name} nerd font"
+#   if [ -n "$font_name" ]; then
+#       echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${font_name}.zip"
+#       curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${font_name}.zip"
+#       echo "creating fonts folder: ~/.local/share/fonts"
+#       mkdir ~/.local/share/fonts
+#       echo "unzip the ${font_name}.zip"
+#       unzip "${font_name}.zip" -d ~/.local/share/fonts/
+#       fc-cache ~/.local/share/fonts
+#       rm -rf "${font_name}.zip"
+#       echo "done!"
       
-      # git clone https://github.com/ryanoasis/nerd-fonts.git fonts --depth 1
-      # cd fonts
-      # ./install.sh DaddyTimeMono
-      # cd .. && rm -rf fonts
-      break
-  fi
-done
+#       # git clone https://github.com/ryanoasis/nerd-fonts.git fonts --depth 1
+#       # cd fonts
+#       # ./install.sh DaddyTimeMono
+#       # cd .. && rm -rf fonts
+#       break
+#   fi
+# done
 
 # install fish shell
 # sudo apt-add-repository ppa:fish-shell/release-3
@@ -37,10 +37,10 @@ done
 # sudo apt install fish
 
 # install ll on steroids
-sudo apt-get update -q --no-allow-insecure-repositories && apt-get install exa -y --no-install-recommends
+sudo apt-get install exa -y
 
 # install stow dotfiles manager
-sudo apt-get update -q --no-allow-insecure-repositories && apt-get install stow -y --no-install-recommends
+sudo apt-get install stow -y
 
 # stow fish from .dotfiles
 stow fish
