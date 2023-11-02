f85ae54aadb6abdb601c7e2a4f280b0458547ab5
Jonathon Perry <Reese.Perry@healthstream.com>
2023-11-02 19:46:42 +0000

    update for consistant fish prompt

--- install.sh
+++ install.sh
@@ -47,6 +47,7 @@ sudo chsh -s $(which fish) $USER
 
 # install fisher fish minimal package manager
 fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update'
+fish -c 'tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Sharp --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Few icons' --transient=Yes'
 
 if [ "${USERNAME}" != "root" ]; then
   su $USERNAME -c 'fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update"'
