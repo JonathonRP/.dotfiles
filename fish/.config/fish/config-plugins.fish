e27797c0c3ddfedde7a016c5de835e3ec42bba06
reese.perry <jreesep@gmail.com>
2023-11-03 18:52:25 +0000

    update

--- fish/.config/fish/config-plugins.fish
+++ fish/.config/fish/config-plugins.fish
@@ -1,5 +1,7 @@
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
+# fundle plugin 'jorgebucaran/fisher'
+fundle plugin 'tuvistavie/oh-my-fish-core'
 fundle plugin 'ilancosman/tide@v6'
 fundle plugin 'jethrokuan/z'
 fundle plugin 'joseluisq/gitnow'
