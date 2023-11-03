e3fe54bf46178446578965715f2d11a6c6ef88d6
Jonathon Perry <Reese.Perry@healthstream.com>
2023-11-03 15:22:08 +0000

    reorder

--- install.sh
+++ install.sh
@@ -31,9 +31,6 @@ do
   fi
 done
 
-# stow from .dotfiles
-stow vim fish --adopt
-
 # ---fish setup---
 # install fish shell
 # sudo apt-add-repository ppa:fish-shell/release-3
@@ -51,6 +48,9 @@ curl -sfL https://git.io/fundle-install | fish && fundle install
 if [ "${USERNAME}" != "root" ]; then
   su $USERNAME -c 'curl -sfL https://git.io/fundle-install | fish && fundle install'
 fi
+
+# stow from .dotfiles
+stow vim fish --adopt
 
 fish -c 'tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Sharp --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Few icons' --transient=Yes'
 # ---vim setup---
