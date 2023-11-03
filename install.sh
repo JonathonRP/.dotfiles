ffa6e2a299e32aa9c1cc536b0a5b04cefa5c2b21
Jonathon Perry <Reese.Perry@healthstream.com>
2023-11-03 15:21:35 +0000

    update streamline

--- install.sh
+++ install.sh
@@ -31,6 +31,9 @@ do
   fi
 done
 
+# stow from .dotfiles
+stow vim fish --adopt
+
 # ---fish setup---
 # install fish shell
 # sudo apt-add-repository ppa:fish-shell/release-3
@@ -43,14 +46,11 @@ done
 sudo chsh -s $(which fish) $USER
 
 # install fisher fish minimal package manager
-fish -c 'curl -sfL https://git.io/fundle-install | fish && fundle install'
+curl -sfL https://git.io/fundle-install | fish && fundle install
 
 if [ "${USERNAME}" != "root" ]; then
-  su $USERNAME -c 'fish -c "curl -sfL https://git.io/fundle-install | fish && fundle install"'
+  su $USERNAME -c 'curl -sfL https://git.io/fundle-install | fish && fundle install'
 fi
-
-# stow from .dotfiles
-stow vim fish --adopt
 
 fish -c 'tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Sharp --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Few icons' --transient=Yes'
 # ---vim setup---
