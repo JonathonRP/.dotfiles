7d45f628e2df03ad22d55648db703041d97a4b7d
Jonathon Reese Perry <jreesep@gmail.com>
2024-09-27 12:54:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,12 +37,10 @@ done
 #   tar xf lazygit.tar.gz lazygit
 #   sudo install lazygit /usr/local/bin
 
-mkdir -p "lazygit"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 echo "downloading lazygit ${LAZYGET_VERSION} at"
-echo curl -Lo lazygit/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-curl -Lo lazygit/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit/lazygit.tar.gz lazygit --overwrite
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar -xzf lazygit.tar.gz lazygit
 echo "installing lazygit"
 sudo install lazygit /usr/local/bin
 
