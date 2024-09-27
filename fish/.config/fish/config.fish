7f77b8752551103f01125b924fbf29d134cf4ddb
Jonathon Reese Perry <jreesep@gmail.com>
2024-09-27 11:53:12 -0500

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,11 +36,6 @@ if test -f $LOCAL_CONFIG
   source $LOCAL_CONFIG
 end
 
-set LAZYGIT_VERSION $(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_LAZYGIT_VERSION""_Linux_x86_64.tar.gz"
-  tar xf lazygit.tar.gz lazygit
-  sudo install lazygit /usr/local/bin
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fisher update
