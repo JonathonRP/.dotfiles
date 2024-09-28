c91df7439abcfa182a0e865619bb470c6c1778c6
Jonathon Reese Perry <jreesep@gmail.com>
2024-09-27 20:51:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ done
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 echo "downloading lazygit ${LAZYGET_VERSION} at"
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit-cli
+tar xf lazygit.tar.gz --directory lazygit-cli
 ls
 echo "installing lazygit-cli"
 sudo install lazygit-cli /usr/local/bin
