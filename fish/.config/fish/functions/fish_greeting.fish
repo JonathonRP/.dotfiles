5c72739800b50c1eaf17ea3bd3e9bd47a91f0029
Jonathon Reese Perry <jreesep@gmail.com>
2024-10-21 19:36:22 +0000

    fix greeting

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,9 +1,9 @@
 function fish_greeting
-  set -l greeting (Wake up, Neo...\n \
+  set -l greeting "(Wake up, Neo...\n \
     The Matrix has you...\n \
     Follow the white rabit.\n \
     \n\n \
-    Knock, knock, Neo.\n | string collect -N)
+    Knock, knock, Neo.\n | string collect -N)"
   set_color green
   for char in (string split -- '' $greeting); echo -ne $char; end
 
