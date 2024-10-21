3d779e976c16eb95296bc1ff786abf43fd4c9c31
Jonathon Reese Perry <jreesep@gmail.com>
2024-10-21 22:11:45 +0000

    tweak

--- install.sh
+++ install.sh
@@ -142,15 +142,13 @@ case $ID in
       echo "done - fisher installed"
     fi
 
-    echo "init fundle"
+    echo "init fundle and update plugins"
 
     fish -c "fundle install"
-    fish -c "fundle update"
+    fish -c "fundle update && fisher update"
 
-    echo "update fisher and config tide"
+    echo "config tide"
 
-    fish -c "fisher update"
-    # fish -c 'tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Sharp --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Few icons' --transient=Yes'
     fish -c "tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Sharp --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Few icons' --transient=Yes"
 
     # ---vim setup---
