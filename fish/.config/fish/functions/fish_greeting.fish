ef408a99c2186d42c2e9ffa61acb1e8fb656674d
Jonathon Reese Perry <jreesep@gmail.com>
2024-10-22 13:43:28 +0000

    fix message and spacing

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,11 +1,15 @@
 function fish_greeting
-  set -l message Wake up, Neo...\n
-    The Matrix has you...\n
-    Follow the white rabit.\n
-    \n\n
-    Knock, knock, Neo.\n\n\n
+  set -l message Wake up, Neo...\n \
+    The Matrix has you...\n \
+    Follow the white rabit.\n \
+    \n \ 
+    \n \
+    Knock, knock, Neo. \
+    \n \
+    \n \
+    \n
   set_color green
-  for char in (string split --allow-empty '' $message); echo -ne $char; end
+  for char in (string split --allow-empty '' $message); printf '%b\n' $char; end
 
   set_color normal
 end
