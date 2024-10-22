870a3f7722aaf4a76fc341efcf2f4600165a01f5
Jonathon Reese Perry <jreesep@gmail.com>
2024-10-22 15:17:28 +0000

    more accurate echo status

--- install.sh
+++ install.sh
@@ -142,7 +142,7 @@ case $ID in
     # ---fish config is now available---
     # fish -c "source ~/.config/fish/config.fish"
 
-    echo "updateing/installing tide and configure tide"
+    echo "installing and configure tide"
     fish -c "fisher update" && fish -c "tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Sharp --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Few icons' --transient=Yes"
 
     exit $?
