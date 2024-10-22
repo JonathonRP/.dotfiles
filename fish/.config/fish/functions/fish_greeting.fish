4f46837d6ab7afa55c351c3f433ec036d77c2a5a
Jonathon Reese Perry <jreesep@gmail.com>
2024-10-22 15:54:52 +0000

    fix install and message format

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,13 +1,13 @@
 function fish_greeting
   set -l message 'Wake up, Neo...
-    The Matrix has you...
-    Follow the white rabit.
-    
-    
-    Knock, knock, Neo.
-    
-    
-    '
+  The Matrix has you...
+  Follow the white rabit.
+  
+  
+  Knock, knock, Neo.
+  
+  
+  '
   set_color green
   for char in (string split '' $message); printf '%b' $char; end
 
